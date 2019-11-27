7418d685b02c48bf555dc38408f75f33cd2fa144
Lúcás Meier <cronokirby@gmail.com>
2019-11-27 14:32:24 +0100

    Add basic type definitions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,119 @@
-export class Foo {}
+type Result<A> = { ok: false; error: string } | { ok: true; value: A };
+
+function mapResult<A, B>(res: Result<A>, f: (a: A) => B): Result<B> {
+  if (res.ok) {
+    return { ...res, value: f(res.value) };
+  } else {
+    return res;
+  }
+}
+
+export interface SerDe<X, A> {
+  encode(entity: X): any;
+  decode(data: any): Result<A>;
+}
+
+class Codec<A, X = A> {
+  constructor(private readonly repr: SerDe<X, A>) {}
+
+  get serde(): SerDe<X, A> {
+    return this.repr;
+  }
+
+  sel<Y>(extract: (big: Y) => X): Codec<A, Y> {
+    const { encode, decode } = this.serde;
+    return new Codec({
+      encode: y => encode(extract(y)),
+      decode,
+    });
+  }
+
+  field(name: string): Codec<A, X> {
+    const { encode, decode } = this.serde;
+    return new Codec({
+      encode: y => ({ [name]: encode(y) }),
+      decode,
+    });
+  }
+}
+
+export const string: Codec<string> = new Codec({
+  encode: s => s,
+  decode: data =>
+    typeof data === 'string'
+      ? { ok: true, value: data }
+      : { ok: false, error: 'expected string' },
+});
+
+export const number: Codec<number> = new Codec({
+  encode: s => s,
+  decode: data =>
+    typeof data === 'number'
+      ? { ok: true, value: data }
+      : { ok: false, error: 'expected number ' },
+});
+
+export const boolean: Codec<boolean> = new Codec({
+  encode: s => s,
+  decode: data =>
+    typeof data === 'boolean'
+      ? { ok: true, value: data }
+      : { ok: false, error: 'expected boolean' },
+});
+
+function encodeRecord<X, R>(
+  x: X,
+  codecs: { [K in keyof R]: Codec<R[K], X> },
+): any {
+  const res: R = {} as R;
+  for (const key in codecs) {
+    res[key] = codecs[key].serde.encode(x);
+  }
+  return res;
+}
+
+function decodeRecord<X, B, R>(
+  data: any,
+  codecs: { [K in keyof R]: Codec<R[K], X> },
+): Result<R> {
+  if (typeof data !== 'object') {
+    return { ok: false, error: 'expected object' };
+  }
+  const res: R = {} as R;
+  for (const key in codecs) {
+    const val = data[key];
+    if (!val) {
+      return { ok: false, error: `missing field ${key}` };
+    }
+    const decode = codecs[key].serde.decode(val);
+    if (!decode.ok) {
+      return decode;
+    }
+    res[key] = decode.value;
+  }
+  return { ok: true, value: res };
+}
+
+export function record<R, X = R>(
+  codecs: { [K in keyof R]: Codec<R[K], X> },
+): Codec<R, X> {
+  return new Codec({
+    encode: x => encodeRecord(x, codecs),
+    decode: data => decodeRecord(data, codecs),
+  });
+}
+
+export function map<A, B, X>(codec: Codec<A, X>, f: (a: A) => B): Codec<B, X> {
+  const { encode, decode } = codec.serde;
+  return new Codec({
+    encode,
+    decode: data => mapResult(decode(data), f),
+  });
+}
+
+export function mapRecord<R, B, X>(
+  codecs: { [K in keyof R]: Codec<R[K], X> },
+  f: (args: R) => B,
+): Codec<B, X> {
+  return map(record(codecs), f);
+}
